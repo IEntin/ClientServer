fbfef787414cd8c2aa619e93e7c531880a919db8
Ilya Entin <ie@gmail.com>
2022-10-25 21:20:53 -0500

    start.sh

--- start.sh
+++ start.sh
@@ -16,13 +16,15 @@ then
     echo "in the ClientOptions.json."
     echo "Start the server in a different shell"
     echo "or in the same shell in the background."
+    echo "Make sure that \"MaxFifoSessions\"" is large
+    echo "enough to accomodate expected number of clients."
     echo "You can now run ./start.sh <N>."
     echo "top or ps shows server and all expected clients running,"
-    echo "all clients consuming roughly the same CPU."
+    echo "clients consuming roughly the same CPU."
     echo "While the script is running you can see generated fifos"
     echo "in the \"FifoDirectoryName\" whatever it is."
-    echo "You can run the same test with \"CommunicationType\" : \"TCP\""
     echo "To stop the script kill the server with Ctrl+C or other signal."
+    echo "You can run similar test with \"CommunicationType\" : \"TCP\""
 
     exit
 fi 
