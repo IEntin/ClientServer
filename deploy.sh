6313f2677ed1f5b578a1192261e2157b8328b88f
IEntin437 <ilya.entin@gmail.com>
2022-12-05 06:19:22 -0600

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@
 if [[ ( $@ == "--help") ||  $@ == "-h" ]]
 then 
     echo "cd to the project root ClientServer and run this script"
-    echo "./deploy.sh 2>&1 | tee deploy.txt"
-    echo "Start the server in the project root terminal ./server"
-    echo "and each client in created terminals ./client"
+    echo "'./deploy.sh 2>&1 | tee deploy.txt'"
+    echo "Start the server in the project root terminal './server'"
+    echo "and each client in client terminals './client'"
     exit 0
 fi 
 
@@ -17,7 +17,7 @@ set -e
 
 # create client directories
 
-# remove existing Client* and Fifos directories to start from scratch
+# remove Client* and Fifos directories to start from scratch
 
 rm -rf ../Fifos
 
@@ -25,15 +25,15 @@ for (( c=1; c<=5; c++ ))do
 rm -rf ../Client$c
 done
 
-# create FIFO directory one level above project root
+# create FIFO directory and client directories at the project root level
+
 mkdir -p ../Fifos
 
-# create client directories one level above project root
 for (( c=1; c<=5; c++ ))do
 mkdir -p ../Client$c
 done
 
-# create links to data directory in every client directory
+# create data directory links in every client directory
 # copy scripts and ClientOptions.json
 
 for (( c=1; c<=5; c++ ))do
@@ -41,14 +41,15 @@ for (( c=1; c<=5; c++ ))do
 done
 
 # now all client directories have the same ClientOptions.json directing to start TCP client
-# if you want to make some of the clients FIFO
+# if you want to make some of the clients, e.g. 2 and 4, FIFO:
 
 for c in 2 4
 do
   (cd ../Client$c;sed -i 's/"CommunicationType" : "TCP"/"CommunicationType" : "FIFO"/' ClientOptions.json)
 done
 
-# build binaries and copy client binary to ClientX directories
+# build binaries and copy client binary to Client* directories
+
 ./compile.sh
 
 # start client terminals
