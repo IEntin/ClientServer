5d40f8ee4924a614320ca935611c72d7bdde4c3d
IEntin437 <ilya.entin@gmail.com>
2023-02-15 22:17:30 -0600

    update

--- ThreadPoolDiffObj.cpp
+++ ThreadPoolDiffObj.cpp
@@ -49,15 +49,13 @@ void ThreadPoolDiffObj::push(RunnablePtr runnable) {
 
 RunnablePtr ThreadPoolDiffObj::get() {
   std::unique_lock lock(_queueMutex);
-  while (true) {
-    _queueCondition.wait(lock, [this] { return !_queue.empty(); });
-    for (auto it = _queue.begin(); it < _queue.end(); ++it) {
-      RunnablePtr runnable = *it;
-      if (runnable->getNumberRunningByType() < runnable->_maxNumberRunningByType) {
-	_queue.erase(it);
-	return runnable;
-      }
+  _queueCondition.wait(lock, [this] { return !_queue.empty(); });
+  for (auto it = _queue.begin(); it < _queue.end(); ++it) {
+    RunnablePtr runnable = *it;
+    if (runnable->getNumberRunningByType() < runnable->_maxNumberRunningByType) {
+      _queue.erase(it);
+      return runnable;
     }
-    return RunnablePtr();
   }
+  return RunnablePtr();
 }
