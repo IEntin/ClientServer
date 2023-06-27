3b90a08561ec44d006f28e1208d223c38d52a8fa
IEntin437 <ilya.entin@gmail.com>
2023-06-27 12:55:52 -0500

    cryptopp lib directory

--- makeCrypto.sh
+++ makeCrypto.sh
@@ -15,14 +15,14 @@ fi
     cd /usr/local
     wget -O https://www.cryptopp.com/$1
     cryptoppBaseName=$(basename $1 .zip)
-    echo "$cryptoppBaseName"
     unzip -aoq $1 -d "$cryptoppBaseName"
+    mkdir -p /usr/local/lib/cryptopp
     cd "$cryptoppBaseName"
     CXX=clang++ make CXXFLAGS="-O3 -fPIC -pipe" -j4
-    cp -f libcryptopp.a /usr/local/lib/libcryptoppclang.a
+    cp -f libcryptopp.a /usr/local/lib/cryptopp/libcryptoppclang.a
     make clean
     CXX=g++ make CXXFLAGS="-O3 -fPIC -pipe" -j4
-    cp -f libcryptopp.a /usr/local/lib/libcryptoppgcc.a
+    cp -f libcryptopp.a /usr/local/lib/cryptopp/libcryptoppgcc.a
     make clean
     ln -sf /usr/local/"$cryptoppBaseName" /usr/local/include/cryptopp
 )
