0310f8fc6dfc4fe126a742ded45a7981b5659530
IEntin437 <ilya.entin@gmail.com>
2023-06-27 11:51:39 -0500

    new crypto++ version etc

--- makeCrypto.sh
+++ makeCrypto.sh
@@ -7,21 +7,22 @@
 if [[ ( $@ == "--help") ||  $@ == "-h" || $# -lt 1 || $# -gt 2 ]]
 then
     echo "Usage: ./makeCrypto.sh <required cryptopp release>"
-    echo "current is cryptopp870.zip"
+    echo "current is cryptopp880.zip"
     exit 0
 fi
 
 (
     cd /usr/local
-    wget https://www.cryptopp.com/$1
-    unzip -aoq cryptopp870.zip -d cryptopp
-    cd cryptopp
-    mkdir /usr/local/include/cryptopp
-    cp *.h /usr/local/include/cryptopp
+    wget -O https://www.cryptopp.com/$1
+    cryptoppBaseName=$(basename $1 .zip)
+    echo "$cryptoppBaseName"
+    unzip -aoq $1 -d "$cryptoppBaseName"
+    cd "$cryptoppBaseName"
     CXX=clang++ make CXXFLAGS="-O3 -fPIC -pipe" -j4
-    cp libcryptopp.a /usr/local/lib/libcryptoppclang.a
+    cp -f libcryptopp.a /usr/local/lib/libcryptoppclang.a
     make clean
     CXX=g++ make CXXFLAGS="-O3 -fPIC -pipe" -j4
-    cp libcryptopp.a /usr/local/lib/libcryptoppgcc.a
+    cp -f libcryptopp.a /usr/local/lib/libcryptoppgcc.a
     make clean
+    ln -sf /usr/local/"$cryptoppBaseName" /usr/local/include/cryptopp
 )
