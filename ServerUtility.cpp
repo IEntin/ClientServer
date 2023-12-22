79ca951d316bb5af284632766f5223c33227afef
IEntin437 <ilya.entin@gmail.com>
2023-12-22 09:48:16 -0600

    ServerUtility

--- ServerUtility.cpp
+++ ServerUtility.cpp
@@ -16,11 +16,11 @@ std::string_view buildReply(const Response& response,
   if (response.empty())
     return {};
   header = { HEADERTYPE::SESSION, 0, 0, ServerOptions::_compressor, ServerOptions::_encrypted, false, status };
-  static thread_local std::vector<char> data;
+  static thread_local std::string data;
   data.resize(0);
   for (const auto& entry : response)
     data.insert(data.end(), entry.begin(), entry.end());
-  return payloadtransform::compressEncrypt({ data.data(), data.size() }, header);
+  return payloadtransform::compressEncrypt(data, header);
 }
 
 bool processTask(const HEADER& header, std::string_view input, TaskPtr task) {
