21a30b15678f9332b494490cf01b28f73b385e4e
IEntin437 <ilya.entin@gmail.com>
2025-02-07 16:11:44 -0600

    help for scripts/runShortSessions.sh script

--- scripts/runShortSessions.sh
+++ scripts/runShortSessions.sh
@@ -8,8 +8,15 @@ set -e
 
 if [[ $@ == "--help" ||  $@ == "-h" || $# -ne 1 ]]
 then
-    echo "Usage: scripts/runShortSessions.sh <times to repeat>"
-    echo "in any or many client directories"
+    echo "Compile the code with any available options."
+    echo "Create a few clients each in its terminal:"
+    echo "'./deploy.sh <number of clients>'"
+    echo "fitting into display. You can run  tcp clients"
+    echo "on different hosts."
+    echo "Start the server './serverX 2>&1 | tee logError.txt'"
+    echo "Run all clients:'scripts/runShortSessions.sh <times to repeat>'"
+    echo "for every client."
+    echo "Check logError.txt for errors or warnings."
     echo "It is a test of server stability under concurrent load from multiple clients"
     echo "of different types frequently creating and destroying sessions. It can be run"
     echo "on binaries built with thread or address sanitizer or on a plain build."
