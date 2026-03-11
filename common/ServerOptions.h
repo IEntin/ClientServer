b59ae809e68136a71d673921b344a54c3d936f9f
IEntin437 <ilya.entin@gmail.com>
2026-03-11 11:13:37 -0500

    restored new features (2)

--- common/ServerOptions.h
+++ common/ServerOptions.h
@@ -11,7 +11,7 @@ enum class COMPRESSORS : char;
 
 struct ServerOptions : Options {
   static void parse(std::string_view jsonName);
-  static std::string _adsFileName;
+  static boost::static_string<100> _adsFileName;
   static COMPRESSORS _compressor;
   static int _compressionLevel;
   static bool _doEncrypt;
@@ -22,8 +22,10 @@ struct ServerOptions : Options {
   static int _tcpTimeout;
   static bool _useRegex;
   static POLICYENUM _policyEnum;
+  static std::size_t _bufferSize;
   static bool _timing;
   static bool _printHeader;
+  static boost::static_string<100> _logThresholdName;
 private:
   ServerOptions() = delete;
   ~ServerOptions() = delete;
--- common/Utility.cpp
+++ common/Utility.cpp
@@ -6,8 +6,6 @@
 #include "Utility.h"
 
 #include <atomic>
-#include <filesystem>
-#include <fstream>
 
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
@@ -27,42 +25,6 @@ std::string generateRawUUID() {
 std::size_t getUniqueId() {
   static std::atomic<size_t> uniqueInteger;
   return uniqueInteger.fetch_add(1);
-}
-
-void readFile(std::string_view fileName, std::string& buffer) {
-  std::ifstream stream;
-  stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  stream.open(fileName.data(), std::ios::binary);
-  std::uintmax_t size = std::filesystem::file_size(fileName);
-  buffer.resize(size);
-  stream.read(buffer.data(), buffer.size());
-}
-
-// used in tests
-bool getLastLine(std::string_view fileName, std::string& lastLine) {
-  char ch;
-  std::ifstream stream;
-  stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  stream.open(fileName.data(), std::ios::binary);
-  stream.seekg(-2, std::ios_base::end);
-  stream.get(ch);
-  while (ch != '\n') {
-    stream.seekg(-2, std::ios_base::cur);
-    stream.get(ch);
-  }
-  std::getline(stream, lastLine);
-  return true;
-}
-
-// used in tests
-bool fileEndsWithEOL(std::string_view fileName) {
-  std::ifstream stream;
-  stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  stream.open(fileName.data(), std::ios::binary);
-  stream.seekg(-1, std::ios::end);
-  char ch;
-  stream.get(ch);
-  return ch == '\n';
 }
 
 void setServerTerminal(std::string_view terminal) {
